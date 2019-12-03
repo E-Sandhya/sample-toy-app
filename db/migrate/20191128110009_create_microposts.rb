4e1cea91903e6d0dab1093d334d6cdd987abe8d7
user <E-Sandhya>
2019-12-03 10:21:25 +0530

    toy-app

---
+++ db/migrate/20191128110009_create_microposts.rb
@@ -0,0 +1,11 @@
+class CreateMicroposts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :microposts do |t|
+      t.text :content
+      t.integer :user_id
+
+      t.timestamps
+    end
+    add_column :microposts, :user_name
+  end
+end
